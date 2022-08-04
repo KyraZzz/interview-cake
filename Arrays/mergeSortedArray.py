7ac46360f5ff8da0158a2563758a7afebe92f9ed
KyraZzz <yz709@cam.ac.uk>
2022-08-05 00:16:38 +0100

    make sure to consider edge cases

--- Arrays/mergeSortedArray.py
+++ Arrays/mergeSortedArray.py
@@ -2,8 +2,8 @@ def mergeSortedArray(l1, l2):
     """
         Args:
             l1 : a list of sorted numbers
-            l2 : a list of sorted numbers 
-        Returns: 
+            l2 : a list of sorted numbers
+        Returns:
             a merged list of sorted numbers, combining l1 and l2
     """
     p1 = 0
@@ -16,12 +16,16 @@ def mergeSortedArray(l1, l2):
         else:
             l.append(l2[p2])
             p2 += 1
+    if p1 < len(l1) - 1:
+        l.extend(l1[p1:])
+    if p2 < len(l2) - 1:
+        l.extend(l2[p2:])
     return l
 
 
 if __name__ == '__main__':
-    my_list = [3, 4, 6, 10, 11, 15]
-    alices_list = [1, 5, 8, 12, 14, 19]
+    my_list = [3, 4, 6, 10, 11, 14]
+    alices_list = [1, 5, 8, 12, 15, 19]
 
     # Prints [1, 3, 4, 5, 6, 8, 10, 11, 12, 14, 15, 19]
     print(mergeSortedArray(my_list, alices_list))
